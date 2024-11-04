f82092eb3dc0f16148b513488af04fc908efcf00
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 13:25:05 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -1,16 +1,24 @@
 
 
 def q1():
-  #Write Assignment code here
+num = input("Input a word: ")
+print (num)
 
 def q2():
-  #Write Assignment code here
+name = input("Input you first name: ")
+print (f"Hello {name}")
 
 def q3():
-  #Write Assignment code here
+name = input("Input you first name: ")
+name1 = input("Input you last name: ")
+print(f"{name1}", end = "")
+print(f" {name}")
 
 def q4():
-  #Write Assignment code here
+stu1 = input("Input a student: ")
+stu2 = input("Input another student: ")
+print (f"Your students are {stu1} and {stu2}")
+
 
 #Do not edit code below this comment
 
