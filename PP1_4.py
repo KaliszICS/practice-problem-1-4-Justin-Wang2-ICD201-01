d76390aa5eecd424b4e115e489d615dc165ab9d9
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 13:28:24 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -1,23 +1,23 @@
 
 
 def q1():
-num = input("Input a word: ")
-print (num)
+  num = input("Input a word: ")
+  print (num)
 
 def q2():
-name = input("Input you first name: ")
-print (f"Hello {name}")
+  name = input("Input you first name: ")
+  print (f"Hello {name}")
 
 def q3():
-name = input("Input you first name: ")
-name1 = input("Input you last name: ")
-print(f"{name1}", end = "")
-print(f" {name}")
+  name = input("Input you first name: ")
+  name1 = input("Input you last name: ")
+  print(f"{name1}", end = "")
+  print(f" {name}")
 
 def q4():
-stu1 = input("Input a student: ")
-stu2 = input("Input another student: ")
-print (f"Your students are {stu1} and {stu2}")
+  stu1 = input("Input a student: ")
+  stu2 = input("Input another student: ")
+  print (f"Your students are {stu1} and {stu2}")
 
 
 #Do not edit code below this comment
